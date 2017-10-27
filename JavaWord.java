f9e02953cd3db205c7ae4b2f23f962fd37f01ab7
SSQ123 <dos1234567890a@gmail.com>
2017-10-27 09:49:02 -0400

    Update JavaWord.java

--- JavaWord.java
+++ JavaWord.java
@@ -1,9 +1,19 @@
 import Javax.swing.*;
+import java.awt.event.*
+
+//Start of code
 public class JavaWord extends JPanel
 {
-  public static main(String[] args) extends JFrame
+  public static main(String[] args) extends JFrame implement ActionListener
   {
   this.setSize(800,600);
   this.setVisible(true);
+  this.add(Menubar);
   } 
+  Public MenuBar extends JFrame
+  {
+  }
+  class BackGroundProcesses
+  {
+  }
 }
